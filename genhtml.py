c1282ef43c76ef3d4000ae023f4873f0e8ff1bb7
Noriyuki Abe <kenko.py@gmail.com>
2015-03-04 14:19:31 +0900

    Modify generated HTML file names and links
    Use HTML filenames and links according to the configuration of config.py

--- genhtml.py
+++ genhtml.py
@@ -72,6 +72,10 @@ class HTMLBuilder:
                      [a_record.ip_address for a_record in a_records]
                      )
                     for ptr_record, a_records in ptr_cor_error
+                ],
+                html_name_addresses=[
+                    (network_address.split("/")[0], network_address)
+                    for network_address in ip_network.keys()
                 ]
             )
             f.write(html)
