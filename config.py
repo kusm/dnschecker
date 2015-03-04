7456f041fc9a13a84dd1db31d32500ab48cfade0
Noriyuki Abe <kenko.py@gmail.com>
2015-03-04 14:48:09 +0900

    Modify config.py to use testzones

--- config.py
+++ config.py
@@ -1,29 +1,27 @@
 #! /usr/bin/env python
 # coding:utf-8
 
-# A レコードのゾーンファイル名
-a_record_filenames = [
-    "math.kyoto-u.ac.jp.zone",
-]
-# PTR レコードのゾーンファイル名そのネットワーク
-ptr_record_filename_networks = [
-    ('10.226.141.rev', '10.226.141.0/24'),
-    ('10.226.142.rev', '10.226.142.0/24'),
-    ('10.226.165.rev', '10.226.165.0/24')
-]
-
-# レコード情報を納めたファイル
-record_info_filenames = [
-    "10.226.142.info"
-]
-
 # レコードファイルがあるディレクトリ
 # デフォルトではこのスクリプトファイルが存在する
 # ディレクトリにある zones/ 以下に配置する
-zone_dir = "zones"
+zone_dir = "testzones"
 
 # 生成した HTML をおくディレクトリ
 html_dir = "build"
 
 # レコードに関する情報をおいているディレクトリ
-record_info_dir = "zones"
+record_info_dir = "testzones"
+
+# A レコードのゾーンファイル名
+a_record_filenames = [
+    "example.jp.zone",
+]
+# PTR レコードのゾーンファイル名そのネットワーク
+ptr_record_filename_networks = [
+    ('192.168.0.rev', '192.168.0.0/24'),
+]
+
+# レコード情報を納めたファイル
+record_info_filenames = [
+    '192.168.0.info',
+]
